392229e1f9a4cc0bbd7825787dcf184f270ae0d3
Anthony <anthonygibson24@gmail.com>
2022-06-04 00:35:35 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const port = 8000;
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.send("Our express api server is now sending this over to the browser");
+});
+  res.json(
+    {message: "cool"}
+  )
+
+const server = app.listen(8000, () =>
+  console.log(`Server is locked and loaded on port ${server.address().port}!`)
+);
+
+app.listen(port, () => { console.log(`Listening on port ${port}`)});
